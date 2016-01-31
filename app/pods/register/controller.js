f0fe833bb6f1c59b7e6f4cfc6299e299335c3606
Jake Lee Kennedy <jake@bodhi.io>
2016-02-01 00:42:26 +0300

    first user to register is a teacher

--- app/pods/register/controller.js
+++ app/pods/register/controller.js
@@ -4,7 +4,7 @@ import EmberValidations from 'ember-validations';
 export default Ember.Controller.extend(EmberValidations, {
   session: Ember.inject.service('session'),
   submitted: false,
-  
+
   validations: {
     'model.firstName': {
       presence: {
@@ -34,11 +34,15 @@ export default Ember.Controller.extend(EmberValidations, {
       }
     }
   },
-  
+
   actions: {
     register() {
       this.set('submitted', true);
       this.validate().then(() => {
+        return this.store.findAll('user');
+      })
+      .then((users) => {
+        if (users.get('length') === 1) this.set('model.isTeacher', true);
         return this.get('model').save();
       })
       .then(() => this.get('session').authenticate('authenticator:local', { id: this.get('model.id'), pin: this.get('model.pin') }))
