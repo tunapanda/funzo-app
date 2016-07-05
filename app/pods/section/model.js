7e107fd0bb081d49d6f14a3848eac002e8d05ca6
Brad Smith <brad@geekdome.net>
2016-07-05 14:37:45 +1000

    force handling expiration as a string

--- app/pods/section/model.js
+++ app/pods/section/model.js
@@ -8,10 +8,6 @@ const ENC_NONE = 0;
 const ENC_AESv2 = 10;
 const ENC_DEFAULT = ENC_AESv2;
 
-function getEncryptionKey() {
-  return  ENV.vendor.encryptionKeyBase;
-}
-
 function getDownloadUrl(bookId) {
   return ENV.vendor.downloadUrlBase;
 }
@@ -30,16 +26,17 @@ export default Model.extend({
     if (typeof(expirationRaw) === "undefined") {
       var expiration = false;
     } else {
+      expirationRaw = expirationRaw.toString();
       var expiration = new Date(
-        expirationRaw.slice(0,4),
-        expirationRaw.slice(4,6),
-        expirationRaw.slice(6,8)
+        expirationRaw.slice(0,4), // YYYY
+        expirationRaw.slice(4,6), // MM
+        expirationRaw.slice(6,8)  // DD
       );
     }
     // This should be changed further down, so if the user
     // ever sees this default, something has indeed gone wrong. 
     let rendered = "<h1>Error</h1><p><em>Failed to extract book content. Please notify your school.</em></p>";
-    if (expiration && expiration > new Date()) {
+    if (expiration && expiration < new Date()) {
       rendered = "<h1>Expired</h1><p><em>Sorry, this copy of " 
         + this.get('book.title')
         + " expired on "
@@ -58,7 +55,7 @@ export default Model.extend({
           ENV.vendor.encryptionKeyBase;
           
          rendered = CryptoJS.AES
-            .decrypt(rendered,encryptionKey)
+            .decrypt(content,encryptionKey)
             .toString(CryptoJS.enc.Utf8);
       }
     }
