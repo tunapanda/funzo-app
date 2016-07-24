e52e1c32e139ff3c8a819ff0fd0731f75344ef72
Brad Smith <brad@geekdome.net>
2016-07-24 12:13:46 -0500

    remove explicit reference to courses

--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -8,8 +8,10 @@ export default Adapter.extend({
   db: db,
   query(store, type, queryObj) {
     return this.findAll(store, type, queryObj).then((results) => {
+      console.log("DBG  query");
+      console.log(results);
       let result = {};
-      result[type.modelName + 's'] = results.courses.filter((item) => Object.keys(queryObj).every((key) => item[key] === queryObj[key]) ? true : false);
+      result[type.modelName + 's'] = results[type.modelName + 's'].filter((item) => Object.keys(queryObj).every((key) => item[key] === queryObj[key]) ? true : false);
       return Ember.RSVP.resolve(results);
     });
   }
