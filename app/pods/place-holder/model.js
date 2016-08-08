c808de412bbae31f862936ab206ae86b891784c2
Jake Lee Kennedy <jake@bodhi.io>
2016-08-08 14:13:41 +0100

    remove placeholder model, no longer needed

--- app/pods/place-holder/model.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-	rev: DS.attr('string'),
-  location: DS.attr('int'),
-  user: DS.belongsTo('user'),
-  book: DS.belongsTo('book')
-});
