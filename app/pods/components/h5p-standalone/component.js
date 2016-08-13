fd49882353d07c0edfec8294590d8cf85f4f67c9
Jake Lee Kennedy <jake@bodhi.io>
2016-08-13 15:49:07 +0100

    h5p activity style improvements
    vertical activity nav on the left
    code the h5p bar at the bottom
    hide the print icon

--- app/pods/components/h5p-standalone/component.js
+++ app/pods/components/h5p-standalone/component.js
@@ -9,13 +9,15 @@ export default Ember.Component.extend({
     h5pContent += `/${this.get('activityPath')}`;
 
     let frameJs = 'assets/h5p-standalone-frame.js';
-    let frameCss = 'assets/h5p.css';
+    let frameCss = 'assets/funzo-app.css';
 
     this.get('currentUser.model').then(() => {
       // if (this.get('activityPath')) {
       //   H5P.jQuery(`#${this.get('activityPath')} .h5p-container`).h5p({ frameJs, frameCss, h5pContent });
       // } else {
       H5P.jQuery(`.h5p-container`).h5p({ frameJs, frameCss, h5pContent });
+
+      // Ember.$(`.h5p-container iframe`).contents().find('head').append(Ember.$('<link/>', { rel: 'stylesheet', href: 'assets/funzo-app.css', type: 'text/css' }));
       // }
 
       H5P.externalDispatcher.on('xAPI', this.xAPI.bind(this));
