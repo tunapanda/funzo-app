05b2093738a822daefdaffc634c200278c7da9fa
Jake Lee Kennedy <jake@bodhi.io>
2017-09-17 22:37:48 +0100

    Add some helpful developer option

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -1,32 +1,38 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
-  tagName: 'nav',
-  classNames: ['navbar', 'navbar-fixed-top'],
+  tagName: "nav",
+  classNames: ["navbar", "navbar-fixed-top"],
 
-  session: Ember.inject.service('session'),
-  nav: Ember.inject.service('nav'),
+  bookManager: Ember.inject.service("bookManager"),
+  session: Ember.inject.service("session"),
+  nav: Ember.inject.service("nav"),
 
-  classNameBindings: ['hide'],
+  classNameBindings: ["hide"],
 
-  showBackButton: Ember.computed.alias('nav.showBackButton'),
-  hide: Ember.computed.alias('nav.hide'),
-  showIcons: Ember.computed.alias('nav.showIcons'),
+  showBackButton: Ember.computed.alias("nav.showBackButton"),
+  hide: Ember.computed.alias("nav.hide"),
+  showIcons: Ember.computed.alias("nav.showIcons"),
 
-
-  bookMode: Ember.computed.alias('nav.hide'),
+  bookMode: Ember.computed.alias("nav.hide"),
 
   actions: {
     back() {
-      this.sendAction('back');
+      this.sendAction("back");
     },
     logout() {
-      this.get('session').invalidate();
+      this.get("session").invalidate();
+    },
+    updateIndex() {
+      this.get("bookManager").updateIndex();
+    },
+    deleteAllBooks() {
+      this.get("bookManager").deleteAll();
     }
   },
 
   didInsertElement() {
-    this.$('.dropdown-button').dropdown();
+    this.$(".dropdown-button").dropdown();
     Ember.$.material.ripples();
   }
 });
